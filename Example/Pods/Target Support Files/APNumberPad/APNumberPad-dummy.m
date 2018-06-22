539a82bc051f64541984928dc28e67525506c251
Andrew Podkovyrin <podkovyrin@gmail.com>
2018-06-22 07:09:19 +0300

    Fixed carthage support

---
+++ Example/Pods/Target Support Files/APNumberPad/APNumberPad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_APNumberPad : NSObject
+@end
+@implementation PodsDummy_APNumberPad
+@end
---
+++ Example/Pods/Target Support Files/APNumberPad/APNumberPad-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "APNumberButton.h"
+#import "APNumberPad.h"
+#import "APNumberPadDefaultStyle.h"
+#import "APNumberPadStyle.h"
+#import "NSBundle+APNumberPad.h"
+
+FOUNDATION_EXPORT double APNumberPadVersionNumber;
+FOUNDATION_EXPORT const unsigned char APNumberPadVersionString[];
+
