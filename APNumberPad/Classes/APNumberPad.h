402dd7971fed5e7e520158e4071a350423f86a94
Zack Sheppard <zack@zacksheppard.com>
2020-07-13 15:47:28 -0400

    Add manual hooks for becoming and losing first responder

--- APNumberPad/Classes/APNumberPad.h
+++ APNumberPad/Classes/APNumberPad.h
@@ -35,6 +35,14 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (strong, readonly, nonatomic) Class<APNumberPadStyle> styleClass;
 
+/**
+ * These methods must be called after the UIResponder using this class as its input view becomes or resigns the first responder.
+ * They will be called automatically if the UIResponder is an instance of UITextField or UITextView, but must be called manually
+ * if it is any other class.
+ */
+- (void)didBecomeActiveInputViewForResponder:(UIResponder<UIKeyInput> *)responder;
+- (void)didResignInputViewForResponder;
+
 @end
 
 ///
